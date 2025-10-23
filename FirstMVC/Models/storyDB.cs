6fc764a73d86479fa1560c73a7324e36fd12a4ae
Jakub Pawel Zych <143728298+s376930@users.noreply.github.com>
2025-10-23 11:29:45 +0200

    fixed StoryDB

--- FirstMVC/Models/storyDB.cs
+++ FirstMVC/Models/storyDB.cs
@@ -17,14 +17,14 @@ namespace FirstMVC.Models
     public List<Choice> Choices { get; set; } = new();
 }
 
-public class Choice {
-    public int Id { get; set; }
-    public string? Text { get; set; }
-    public int NextActId { get; set; }
-    public bool IsCorrect { get; set; }
+    public class Choice{
+        public int Id { get; set; }
+        public string? Text { get; set; }
+        public int NextActId { get; set; }
+        public bool IsCorrect { get; set; }
 
-    public int StoryActId { get; set; }
-    public StoryAct StoryAct { get; set; } = null!;
-}
+        public int StoryActId { get; set; }
+        public StoryAct StoryAct { get; set; } = null!;
+    }
 
 }
