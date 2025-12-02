d8ec920ea27139ae81d2a6866491877a1914440c
Jakub Pawel Zych <143728298+s376930@users.noreply.github.com>
2025-12-02 12:01:59 +0100

    gave the NPC's more correct names and roles

--- FirstMVC/Controllers/SeedController.cs
+++ FirstMVC/Controllers/SeedController.cs
@@ -22,7 +22,8 @@ namespace FirstMVC.Controllers
                 new Characters
                 {
                     Name = "Friend 1",
-                    Role = "ID_FRIEND1",
+                    Role = "Female Friend",
+                    CharacterCode = "ID_FRIEND1",
                     Description = "Your first friend in the story",
                     Dialog = "",
                     ImageUrl = "",
@@ -31,7 +32,8 @@ namespace FirstMVC.Controllers
                 new Characters
                 {
                     Name = "Friend 2",
-                    Role = "ID_FRIEND2",
+                    Role = "Male Friend",
+                    CharacterCode = "ID_FRIEND2",
                     Description = "Your second friend in the story",
                     Dialog = "",
                     ImageUrl = "",
@@ -40,7 +42,8 @@ namespace FirstMVC.Controllers
                 new Characters
                 {
                     Name = "Parent",
-                    Role = "ID_PARENT",
+                    Role = "Parent",
+                    CharacterCode = "ID_PARENT",
                     Description = "The parent character",
                     Dialog = "",
                     ImageUrl = "",
@@ -49,7 +52,8 @@ namespace FirstMVC.Controllers
                 new Characters
                 {
                     Name = "Principal",
-                    Role = "ID_PRINCIPAL",
+                    Role = "Principal",
+                    CharacterCode = "ID_PRINCIPAL",
                     Description = "The school principal",
                     Dialog = "",
                     ImageUrl = "",
